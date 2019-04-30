c47b11f007ca8dacb2609ed8577c521d068d8dee
Charles Clemens <cclemensjr@gmail.com>
2019-04-30 13:44:37 -0700

    Created routes

--- server.js
+++ server.js
@@ -1,57 +1,72 @@
 'use strict';
 
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const cors = require('cors');
+/****************
+ * Configure Middleware
+ */
+  require('dotenv').config();
 
-let locationObject;
-let weatherArray = [];
-const port = process.env.PORT || 3000;
-app.use(cors());
+  const express = require('express');
+  const app = express();
+  const cors = require('cors');
+  const port = process.env.PORT || 3000;
+  // let locationObject;
+  // let weatherArray = [];
+  app.use(cors());
+  app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 
 
-function Location (search_query, formatted_query, latitude, longitude){
-  this.search_query = search_query;
-  this.formatted_query = formatted_query;
-  this.latitude = latitude;
-  this.longitude = longitude;
-}
-function Weather (forecast, time){
-  this.summary = forecast;
-  this.time = time;
-  weatherArray.push(this);
-}
+/****************
+ * Create Routes
+ */
+  app.get('/location', getLocation);
+  app.get('/weather', getWeather);
 
-app.get('/location', (request, response) => {
-  let dataFile = require('./data/geo.json');
-  let search_query = dataFile.results[0].address_components[0].long_name;
-  let formatted_query = dataFile.results[0].formatted_address;
-  let latitude = dataFile.results[0].geometry.location.lat;
-  let longitude = dataFile.results[0].geometry.location.lng;
-  locationObject = new Location(search_query, formatted_query, latitude, longitude);
-  response.status(200).send(locationObject);
 
-});
 
-app.get('/weather', (request, response) => {
-  weatherArray = [];
-  let dataFile = require('./data/darksky.json');
-  let weatherForecast = dataFile.daily.data[0];
-  // for(let i =0; i < weatherForecast.length; i++){
-  //   new Weather(weatherForecast.summary, new Date(weatherForecast.time));
+/****************
+ * Callback Functions
+ */
+  
+
+/****************
+ * Object Constructors
+ */
+  // function Location(data) {
+  //   this.formatted
   // }
-  response.status(200).send(weatherForecast);
-});
+//   function Location (search_query, formatted_query, latitude, longitude){
+//   this.search_query = search_query;
+//   this.formatted_query = formatted_query;
+//   this.latitude = latitude;
+//   this.longitude = longitude;
+// }
+// function Weather (forecast, time){
+//   this.summary = forecast;
+//   this.time = time;
+//   weatherArray.push(this);
+// }
+
 // app.get('/location', (request, response) => {
-//   let LongNLats = {
-//     departure: Date.now(),
-//     canFly: true,
-//     pilot: 'Well Trained'
-//   }
-//   response.status(200).json(airplanes);
+//   let dataFile = require('./data/geo.json');
+//   let search_query = dataFile.results[0].address_components[0].long_name;
+//   let formatted_query = dataFile.results[0].formatted_address;
+//   let latitude = dataFile.results[0].geometry.location.lat;
+//   let longitude = dataFile.results[0].geometry.location.lng;
+//   locationObject = new Location(search_query, formatted_query, latitude, longitude);
+//   response.status(200).send(locationObject);
+
 // });
 
-app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
+// app.get('/weather', (request, response) => {
+//   weatherArray = [];
+//   let dataFile = require('./data/darksky.json');
+//   let weatherForecast = dataFile.daily.data[0];
+//   // for(let i =0; i < weatherForecast.length; i++){
+//   //   new Weather(weatherForecast.summary, new Date(weatherForecast.time));
+//   // }
+//   response.status(200).send(weatherForecast);
+// });
+
+
 
 app.listen(port,() => console.log(`Listening on port ${port}`));
